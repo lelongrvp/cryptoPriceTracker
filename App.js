7746444d4ec3be1125e76da67b3edaa853125876
poochswadge <lelongrvp7@gmail.com>
2022-05-14 12:23:19 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ListItem from './components/ListItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleWrapper}>
+        <Text style={styles.largeTitle}>Market</Text>
+      </View>
+      <View style={styles.divider}/>
+      <ListItem/>
     </View>
   );
 }
@@ -14,7 +19,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  titleWrapper: {
+    marginTop: 80,
+    paddingHorizontal: 16,
+  },
+  largeTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: "#A9ABB1",
+    marginHorizontal: 16,
+    marginTop: 16,
+  }
 });
---
+++ components/ListItem.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ListItem = () => {
+  return (
+    <View>
+      <Text>List Item</Text>
+    </View>
+  )
+}
+
+export default ListItem
